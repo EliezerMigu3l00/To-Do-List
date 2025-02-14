b2f64d3505b66f2198a31299f0b0d65528059963
EliezerMigu3l00 <eliezermiguel97@outlook.com>
2025-02-14 19:51:49 -0300

    Projeto finalizado!!!

--- todos/models.py
+++ todos/models.py
@@ -1,8 +1,18 @@
+from datetime import date
+
 from django.db import models
 
 
 class Todo(models.Model):
-    title = models.CharField(max_length=100, null=False, blank=False)
+    title = models.CharField(verbose_name="Titulo",max_length=100, null=False, blank=False)
     created_at = models.DateTimeField(auto_now_add=True, null=False, blank=False)
-    deadline = models.DateTimeField(null=False, blank=False)
-    fineshed_at = models.DateTimeField(null=True)
+    deadline = models.DateField(verbose_name="Data de Entrega",null=False, blank=False)
+    fineshed_at = models.DateField(null=True)
+
+    class Meta:
+        ordering = ["deadline"]
+
+    def mark_has_complete(self):
+        if not self.fineshed_at:
+            self.fineshed_at = date.today()
+            self.save()
